40f4fa34f9bb312869576c0e7342600be3ef56eb
Toleutai Bekzat <toleutaib@gmail.com>
2019-10-07 21:23:16 +0600

    Задание 1: прочитал все меоды Console и вывел на консоль

---
+++ Reflection/Reflection/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflection
+{
+  public class Person
+  {
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+    public bool Gender { get; set; }
+  }
+}
---
+++ Reflection/Reflection/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection
+{
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Task 1: ");
+      DemoTask1();
+      Console.WriteLine("\n\n\n\nTask 2:");
+      DemoTask2();
+      Console.ReadLine();
+    }
+
+    static void DemoTask2()
+    {
+      var person = new Person
+      {
+        Name = "Bekzat",
+        Surname = "Toleutai",
+        Age = 16,
+        Gender = true,
+      };
+
+      var PersonProperties = typeof(Person).GetProperties();
+
+      foreach (var property in PersonProperties)
+      {
+        Console.WriteLine($"{property.Name}:{property.GetValue(person)}");
+      }
+    }
+
+    static void DemoTask1()
+    {
+      var methodInfo = typeof(Console).GetMethods();
+
+      foreach (var methodName in methodInfo)
+      {
+        Console.WriteLine(methodName);
+      }
+    }
+  }
+}
